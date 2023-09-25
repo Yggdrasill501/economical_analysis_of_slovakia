1e69f419b2883eb6eb8c5f43922cbf426419a3cc
Filip Žitný <filipzitny@gmail.com>
2023-09-25 13:25:34 +0200

    webscraping

--- get_data/get_gdp.py
+++ get_data/get_gdp.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """File for getting GDP data from World Bank API."""
 import scrapy
+import json
 
 
 class GetGDP:
@@ -10,4 +11,15 @@ class GetGDP:
 
         :rtype None:
         """
-        self._api = ''
+        self._api = 'http://api.worldbank.org/v2/country/br/indicator/NY.GDP.MKTP.CD?format=json'
+        self._url = scrapy.Request(self._api)
+        self._data = scrapy.urlopen(self._url).read()
+        self._gdp = json.loads(self._data)
+
+    def get_data(self) -> None:
+        """Get data from World Bank API.
+
+        :rtype None:
+        """
+        print(self._gdp)
+        return self._gdp
