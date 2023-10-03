e4ece74695d96c62e936846fa92f441e966cfd53
Filip Žitný <filipzitny@gmail.com>
2023-10-03 18:55:17 +0200

    eurostat communication

---
+++ eurostat_api/get_data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""File for downloading data from Eurostat."""
+import eurostat
+import scrapy
+
+
+class DownloadData:
+    """Class for downloading data from Eurostat."""
+    def __init__(self) -> None:
+        """Constructor.
+
+        rtype None:
+        """
